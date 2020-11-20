d2afe8a304b03598490f04ab69d595166411a813
Regiane Martins de Brito <martins.regiane.silva@gmail.com>
2020-11-20 09:46:19 -0300

    First commit

---
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var contexto = new LojaContext())
+            {
+                var produtos = contexto.Produtos.ToList();
+                foreach (var p in produtos)
+                {
+                    Console.WriteLine(p);
+                }
+
+                Console.WriteLine("=====================");
+                foreach (var e in contexto.ChangeTracker.Entries())
+                {
+                    Console.WriteLine(e.State);
+                }
+                
+
+                //var p1 = produtos.First();
+                //p1.Nome = "Harry Potter e a Pedra Filosofal";
+
+                //contexto.SaveChanges();
+
+                Console.WriteLine("=====================");
+                produtos = contexto.Produtos.ToList();
+                foreach (var p in produtos)
+                {
+                    Console.WriteLine(p);
+                }
+            }
+        }
+
+       
+    }
+}
